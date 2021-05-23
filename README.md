db931f668837cb133feb7549939f12405f417359
humandotlearning <humandotlearning@gmail.com>
2021-05-23 21:18:54 +0530

    + basic program

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# docker_fastapi_redis
+# setup
+
+```
+sudo apt-get install redis-server
+pip install -r requirements.txt
+```
+
+vscode extension used to create docker: 
+> ms-azuretools.vscode-docker
+
+# to Run
+
+```
+python -m uvicorn main:app --reload
+```
+
+References:
+1. [Working with Docker and VSCode](https://www.youtube.com/watch?v=wUUmRbXiIOo)
---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+import os 
+import redis 
+import socket
+app = FastAPI()
+redis_host = os.environ.get("REDIS_HOST") or "host.docker.internal"
+cache = redis.client.Redis(host=redis_host)
+
+@app.get("/")
+
+async def root():
+    return {
+        "hello" : "world",
+        "hits" : cache.incr("hits"),
+        "app_hostname": socket.gethostname(),
+        "redis_hostname": redis_host
+    }
